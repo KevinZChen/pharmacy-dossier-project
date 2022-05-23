3462ec0e7d46c2e1e6e477c83dea56076b0fda78
mudkipmilk <97408222+mudkipmilk@users.noreply.github.com>
2022-05-22 21:53:03 -0400

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function App() {
+  return (
+    <div>
+      <div>
+        <h1>My Dossier</h1>
+      </div>
+      <input></input>
+      <button>add</button>
+    </div>
+  );
+}
+export default App;
